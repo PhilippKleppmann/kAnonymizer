f815be3472d8508f1dd9556a325f7f6746fd1f47
Philipp Kleppmann <philipp.kleppmann@tngtech.com>
2020-04-01 22:35:19 +0200

    test that quasi-identifier columns can be mixed with non-identifier columns

--- src/test/java/anonymization/kAnonymizerTest.java
+++ src/test/java/anonymization/kAnonymizerTest.java
@@ -1,5 +1,7 @@
 package anonymization;
 
+import dto.Dataset;
+import dto.NonIdentifiers;
 import dto.QuasiIdentifiers;
 import java.io.IOException;
 import java.util.Arrays;
@@ -21,4 +23,21 @@ public class kAnonymizerTest {
         assertEquals(1, frequencyList.getData().get(new QuasiIdentifiers(Arrays.asList("Alice", "25"))).size());
     }
 
+    @Test
+    public void testLoadDataWithMixedQuasiAndNonIdentifiers() throws IOException {
+        final KAnonymizer kAnonymizer = new KAnonymizer(resourcesPrefix + "quasi-identifiers/zeroAndTwo.csv");
+
+        final FrequencyList frequencyList = kAnonymizer.loadData(resourcesPrefix + "data/testData4Columns.csv");
+
+        final Dataset expectedDataset = new Dataset();
+        final QuasiIdentifiers alice = new QuasiIdentifiers(Arrays.asList("Alice", "32"));
+        final NonIdentifiers aliceNonIdentifiers = new NonIdentifiers(Arrays.asList("X", "XX"));
+        final QuasiIdentifiers bob = new QuasiIdentifiers(Arrays.asList("Bob", "14"));
+        final NonIdentifiers bobNonIdentifiers = new NonIdentifiers(Arrays.asList("Y", "YY"));
+        expectedDataset.add(alice, aliceNonIdentifiers);
+        expectedDataset.add(bob, bobNonIdentifiers);
+
+        assertEquals(2, frequencyList.getData().size());
+        assertEquals(expectedDataset, frequencyList.getData());
+    }
 }
