933b31867d4b1e8454b8ba62f3db61c8cfd88601
Philipp Kleppmann <philipp.kleppmann@tngtech.com>
2020-04-01 22:15:51 +0200

    combine writing tests with reading tests to be able to assert successful writing

--- src/test/java/io/CsvHandlerTest.java
+++ src/test/java/io/CsvHandlerTest.java
@@ -15,29 +15,31 @@ import static org.junit.Assert.assertTrue;
 
 public class CsvHandlerTest {
 
-    private final  String       resourcesPrefix = "src/test/resources/";
-    private static List<String> alice           = Arrays.asList("Alice", "25");
+    private final String       resourcesPrefix = "src/test/resources/";
+    private final List<String> alice           = Arrays.asList("Alice", "25");
 
     @Test
-    public void testReadCsv() throws IOException {
-        final List<List<String>> csv = CsvHandler.readCsv(resourcesPrefix + "data/testData1Row.csv");
-        assertEquals(1, csv.size());
-        assertEquals(alice, csv.get(0));
-    }
-
-    @Test
-    public void testWriteCsv() throws IOException {
+    public void testReadWriteCsv() throws IOException {
         List<List<String>> data = Arrays.asList(alice);
+
         CsvHandler.writeCsv(resourcesPrefix + "data/testWriteCsv.csv", data);
+        final List<List<String>> csv = CsvHandler.readCsv(resourcesPrefix + "data/testWriteCsv.csv");
+
+        assertEquals(1, csv.size());
     }
 
     @Test
-    public void testWriteDataset() throws FileNotFoundException, UnsupportedEncodingException {
+    public void testReadWriteDataset() throws IOException {
         final Dataset dataset = new Dataset();
         final QuasiIdentifiers quasiIdentifiers = new QuasiIdentifiers(Arrays.asList("a", "b"));
         final NonIdentifiers nonIdentifiers = new NonIdentifiers(Arrays.asList("c", "d"));
         dataset.add(quasiIdentifiers, nonIdentifiers);
+
         CsvHandler.writeCsv(resourcesPrefix + "data/testWriteDataset.csv", dataset);
+        final List<List<String>> csv = CsvHandler.readCsv(resourcesPrefix + "data/testWriteDataset.csv");
+
+        assertEquals(1, csv.size());
+        assertEquals(Arrays.asList("a","b","c","d"), csv.get(0));
     }
 
     @Test
